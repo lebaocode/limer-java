beee394cd6fc7982c9bdcf4ed099ac19c76cd2db
lebaocode <lebao_code@163.com>
2018-10-08 22:28:19 +0800

    MOD:add json function

--- src/main/java/com/lebaor/thirdpartyutils/DoubanUtil.java
+++ src/main/java/com/lebaor/thirdpartyutils/DoubanUtil.java
@@ -8,7 +8,8 @@ import com.lebaor.webutils.HttpClientUtil;
 public class DoubanUtil {
 	public static String getBookInfoByIsbn(String isbn) {
 		String json = HttpClientUtil.doGet("https://api.douban.com/v2/book/isbn/:" + isbn);
-		LogUtil.WEB_LOG.debug("DoubanUtil.getBookInfoByIsbn(" +isbn+"), return " + json);
+		LogUtil.WEB_LOG.debug("DoubanUtil.getBookInfoByIsbn(" +isbn+"), return " 
+				+ (json.length() > 0 ? json.substring(0, Math.min(json.length(), 100)): ""));
 		return json;
 	}
 }
